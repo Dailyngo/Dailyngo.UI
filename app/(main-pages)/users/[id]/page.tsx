6a0e3f9d83c2f30ced9a063207e49abd5836b00f
İbrahim Halil Ateş <atesibrahim612@gmail.com>
2025-04-23 19:14:33 +0300

    hotfix

--- app/(main-pages)/users/[id]/page.tsx
+++ app/(main-pages)/users/[id]/page.tsx
@@ -2,13 +2,25 @@
 import React from "react";
 
 import UserProfile from "@/components/profile/userProfile";
+import { getTokenInfos } from "@/utils/helpers";
+import { useRouter } from "next/navigation";
 
 const UserProfilePage = ({ params }: { params: { id: string } }) => {
+  const loginUserDetail = getTokenInfos();
+  const loginUserId = loginUserDetail?.sub;
+
+  if(loginUserId == params.id) {
+    const router = useRouter();
+    router.push("/profile");
+
+    return <></>;
+  }
+  else
     return (
       <div className="min-h-screen bg-gray-50">
         <UserProfile userId={params.id} /> 
       </div>
     );
-  };
+};
   
   export default UserProfilePage;
