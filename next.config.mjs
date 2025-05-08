9713a66e8f91abebb703fc9cf275a0f55469cd73
İbrahim Halil Ateş <atesibrahim612@gmail.com>
2025-05-08 18:14:27 +0300

    mobil uygulama destegi eklendi

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 import dotenv from "dotenv";
+import withPWA from 'next-pwa';
 dotenv.config({ path: "./.env" });
-const nextConfig = {
+
+const nextConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})({
   reactStrictMode: false,
   compress: true,
   output: undefined,
@@ -9,7 +16,6 @@ const nextConfig = {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
-
-};
+});
 
 export default nextConfig;
